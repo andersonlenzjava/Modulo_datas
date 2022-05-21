20d504f3fdf7c88688a2a9bbf23114ccbf8aca97
ander <ander@DESKTOP-G3BCR3T>
2022-05-21 19:06:11 -0300

    Datas e hora

---
+++ github.com/andersonlenzjava/Modulo_datas.git/Modulo_datas_aula/src/modulo_datas/DatasEmJava.java
@@ -0,0 +1,28 @@
+package modulo_datas;
+
+import java.util.Date;
+
+public class DatasEmJava {
+	
+	//usando a API datas
+	
+	public static void main(String[] args) {
+		
+		Date date = new Date();//Instanciação do objeto da classe que recebe os dados 
+		//métodos desta classe (ler a documentação de cada método para saber o que faz)
+		System.out.println(" Data em milisegundos: " + date.getTime());//quando vc aperta o espaço aparece o tipo de váriavel que retorna
+		
+		System.out.println(" Dia do mês: " + date.getDate());
+		
+		System.out.println(" Dia da semana: " + date.getDay());
+		
+		System.out.println(" Hora do dia:  " + date.getHours());
+		
+		System.out.println(" Minuto da hora:  " + date.getMinutes());
+		
+		System.out.println(" Segundos:  " + date.getSeconds());
+		
+		System.out.println(" Ano:  " + (1900 + date.getYear()));//ano base 1900
+	}
+
+}
